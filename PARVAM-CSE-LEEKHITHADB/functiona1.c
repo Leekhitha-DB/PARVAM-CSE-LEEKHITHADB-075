e0c3d2965592f0598b1cc934339c4c33ebe5fac1
Leekhitha-DB <leekhithadb@gmail.com>
2024-11-06 14:21:51 +0530

    C-NOV6

---
+++ PARVAM-CSE-LEEKHITHADB/functiona1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void add(int a,int b)
+{
+    printf("%d\n",a+b);
+}
+int main()
+{
+    add(5,6);
+    add(10,20);
+    add(5,10);
+    return 0;
+}    
---
+++ PARVAM-CSE-LEEKHITHADB/functiona2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int add(int a,int b)
+{
+    printf("%d\n",a+b);
+}
+int main()
+{
+    add(9,5);
+    return 0;
+}    
